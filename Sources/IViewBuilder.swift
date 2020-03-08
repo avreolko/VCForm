ded08835fdf475cd106db1d5c24eaca350b12910
avreolko <vacherep@gmail.com>
2020-03-08 18:06:34 +0700

    added generic view builder

--- Sources/IViewBuilder.swift
+++ Sources/IViewBuilder.swift
@@ -22,3 +22,18 @@ public protocol IViewBuilder {
     associatedtype View: UIView
     func buildView() -> View
 }
+
+public struct Builder<View: UIView>: IViewBuilder {
+
+    private let viewHandler: ((View) -> Void)?
+
+    public init(viewHandler: ((View) -> Void)? = nil) {
+        self.viewHandler = viewHandler
+    }
+
+    public func buildView() -> View {
+        let view = View(frame: .zero)
+        self.viewHandler?(view)
+        return view
+    }
+}
