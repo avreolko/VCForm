d671d12b03f3d3f81060fb666a38c53725fc2dbb
Валентин Черепянко <v.cherepjanko@cft.ru>
2020-03-20 14:29:57 +0700

    added generic xib builder

--- Sources/IViewBuilder.swift
+++ Sources/IViewBuilder.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VCExtensions
 
 public protocol IdentifiableView: AnyObject {
     var id: String { get }
@@ -33,6 +34,25 @@ public struct Builder<View: UIView>: IViewBuilder {
 
     public func buildView() -> View {
         let view = View(frame: .zero)
+        self.viewHandler?(view)
+        return view
+    }
+}
+
+public struct XibBuilder<View: UIView>: IViewBuilder {
+
+    private let viewHandler: ((View) -> Void)?
+
+    public init(viewHandler: ((View) -> Void)? = nil) {
+        self.viewHandler = viewHandler
+    }
+
+    public func buildView() -> View {
+
+        guard let view: View = View.loadFromXib() else {
+            fatalError("There is no .xib file with name: \(String(describing: View.self))")
+        }
+
         self.viewHandler?(view)
         return view
     }
